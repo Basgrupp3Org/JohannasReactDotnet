81e5a87e6bf9cbd8f2c4ee893524d1fd9bf41037
karrelo <kardo.rahim@hotmail.se>
2021-10-20 10:19:39 +0200

    first

---
+++ ClientApp/src/components/Home/HomePage.js
@@ -0,0 +1,34 @@
+﻿import React, { useEffect, useState } from 'react'
+import authService from '../api-authorization/AuthorizeService'
+
+
+export default function HomePage() {
+    const [budgetData, setBudgetData] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchMyAPI() {
+           
+
+            const token = await authService.getAccessToken();
+            const response = await fetch('api/Budget', {
+                headers: !token ? {} : { 'Authorization': `Bearer ${token}` }
+            });
+            const data = await response.json();
+            setBudgetData(data)
+        }
+
+        fetchMyAPI()
+
+
+    }, []);
+
+    return (
+        <div className="homepage__fullPage_div">
+
+            <div>{budgetData ? "finns" : "finns inte"}</div>
+        </div>
+
+
+    )
+}
